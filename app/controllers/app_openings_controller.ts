e76f0c77620ed7ca080458fd13359437e4c4190f
Maxime Bégué <begue.maxime@hotmail.fr>
2024-11-02 17:19:15 +0100

    refacto(endpoint increment) : service creation, code refactored.

--- app/controllers/app_openings_controller.ts
+++ app/controllers/app_openings_controller.ts
@@ -1,16 +1,34 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import AppOpening from '#models/app_opening'
+import { inject } from '@adonisjs/core'
+import { AppOpeningService } from '../app_opening_service.js'
 
+@inject()
 export default class AppOpeningsController {
-    public async increment({ response }: HttpContext) {
-        let appOpening = await AppOpening.first()
-        if (!appOpening) {
-            appOpening = new AppOpening()
-            appOpening.count = 1
-        } else {
-            appOpening.count += 1
-        }
-        await appOpening.save()
-        return response.status(200).json({ success: true, count: appOpening.count })
+  constructor(private appOpeningService: AppOpeningService) {}
+
+  /**
+   * Increments the number of visits to the app
+   */
+  public async increment({ response }: HttpContext) {
+    const appOpened = await this.appOpeningService.increment()
+    return response.ok({ code: response.getStatus(), appOpened })
+  }
+
+  /**
+   * @Returns the number of visits to the app
+   */
+  public async count({ response }: HttpContext) {
+    try {
+      const count = await this.appOpeningService.count()
+      return response.status(200).send({ count })
+    } catch (error) {
+      if (error.code === 'E_ROW_NOT_FOUND') {
+        return { count: 0 }
+      }
+
+      return response.status(500).send({
+        message: 'Something went wrong',
+      })
     }
+  }
 }
